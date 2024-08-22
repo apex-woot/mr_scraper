91338a1e7a0a005d497f809f95cbbc25b2e9031a
Vitalii Kohut <vkohut@intelliarts.com>
2024-08-22 17:11:12 +0300

    feat: add dockerfile

--- cmd/main.go
+++ cmd/main.go
@@ -1,45 +1,7 @@
 package main
 
-import (
-	"context"
-	"log"
-
-	"github.com/apexwoot/mr_scraper/config"
-	"github.com/apexwoot/mr_scraper/internal/app"
-)
+import "github.com/apexwoot/mr_scraper/internal/scraper"
 
 func main() {
-	if err := config.LoadEnv(); err != nil {
-		log.Fatalf("Error loading env: %s", err)
-	}
-	dbConn, err := config.NewConnection(config.ComposeDBConnectionString())
-	if err != nil {
-		log.Fatalf("Error while creating DB connection: %v", err)
-	}
-	defer dbConn.Disconnect(context.TODO())
-
-	newestPost, err := dbConn.GetNewestPost()
-	if err != nil {
-		log.Fatalf("Error retrieving newest post: %v", err)
-	}
-	newestCheckedPostTimestamp := app.GetNewestPostTimestampOrDefault(newestPost)
-
-	doc, err := app.FetchHTML("https://www.reddit.com/r/mechmarket/search/?q=mode+envoy&sort=new")
-	if err != nil {
-		log.Fatalf("Error fetching HTML: %s", err)
-	}
-
-	element := app.FindTagByName(doc, "reddit-feed")
-	posts, err := app.GetChildren(element, newestCheckedPostTimestamp)
-	if err != nil {
-		log.Fatalf("Error while retrieving posts: %v", err)
-	}
-
-	log.Printf("Found %d new posts. Saving...", len(posts))
-
-	_, err = dbConn.SavePosts(posts)
-	if err != nil {
-		log.Fatalf("Error saving posts: %v", err)
-	}
-	log.Printf("Saved %d new posts.", len(posts))
+	scraper.Scrape()
 }
